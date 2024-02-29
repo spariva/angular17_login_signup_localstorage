5e84a5c9a4efcf8cb355f84daa0f348764fca486
Sergio :) <sergio.serrano3@educa.madrid.org>
2024-02-29 17:14:55 +0100

    para presentar

--- src/app/pages/consultas/consultas.component.ts
+++ src/app/pages/consultas/consultas.component.ts
@@ -1,32 +1,80 @@
-import { CommonModule, NgFor } from '@angular/common';
 import { Component, OnInit, inject } from '@angular/core';
+//import clientes from '../../assets/datos.json';
 import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-consultas',
   templateUrl: './consultas.component.html',
   styleUrl: './consultas.component.css'
 })
-export class ConsultasComponent implements OnInit{
+export class ConsultasComponent{
+  // public listaClientes:{id:number, nombre:string, apellido:string}[] = clientes;
+  // http = inject(HttpClient);
+  // clientes:any = [];
+  clientes: any = [
+    {"nombre": "AAAAAAAAAAAAAA", "apellido": "perez"},
+    {"nombre": "Maria", "apellido": "merlo"},
+    {"nombre": "Carlos", "apellido": "perez"},
+    {"nombre": "Ana", "apellido": "sanchez"},
+    {"nombre": "Pedro", "apellido": "gomez"},
+]
 
-  http = inject(HttpClient);
-  clientes:any = [];
-  mostrar = false;
-  
-  ngOnInit(): void {
-      this.fetchClientes();
+  loggedUser: any;
+  constructor(private router: Router) {
+    const localUser = localStorage.getItem('loggedUser');
+    if(localUser != null) {
+      this.loggedUser = JSON.parse(localUser);
+    }
   }
 
-  fetchClientes(){
-    this.http.post('http://localhost:3001/cliente', {id:1})
-    .subscribe((clientes: any) =>{
-      console.log(clientes);
-      // this.clientes = clientes;
-      if(typeof this.clientes == "object" ){
-        this.clientes = clientes;
-        console.log(typeof this.clientes);
-        console.log(this.clientes.length)
-      }    
-    })
+  pintar(){
+    console.log(this.clientes);
+    localStorage.setItem('clientes', JSON.stringify(this.clientes));
   }
+  // ngOnInit(): void {
+  //     this.fetchClientes();
+  // }
+
+  // fetchClientes(){
+  //   this.http.get('http://localhost:3001/consultas')
+  //   .subscribe((clientes: any) =>{
+  //     console.log(clientes);
+  //     this.clientes = clientes;
+  //   })
+  // }
 }
+
+// import { CommonModule, NgFor } from '@angular/common';
+// import { Component, OnInit, inject } from '@angular/core';
+// import { HttpClient } from '@angular/common/http';
+// import { Router } from '@angular/router';
+
+// @Component({
+//   selector: 'app-consultas',
+//   templateUrl: './consultas.component.html',
+//   styleUrl: './consultas.component.css'
+// })
+// export class ConsultasComponent implements OnInit{
+
+//   http = inject(HttpClient);
+//   clientes:any = [];
+//   mostrar = false;
+  
+//   ngOnInit(): void {
+//       this.fetchClientes();
+//   }
+
+//   fetchClientes(){
+//     this.http.post('http://localhost:3001/cliente', {id:1})
+//     .subscribe((clientes: any) =>{
+//       console.log(clientes);
+//       // this.clientes = clientes;
+//       if(typeof this.clientes == "object" ){
+//         this.clientes = clientes;
+//         console.log(typeof this.clientes);
+//         console.log(this.clientes.length)
+//       }    
+//     })
+//   }
+// }
